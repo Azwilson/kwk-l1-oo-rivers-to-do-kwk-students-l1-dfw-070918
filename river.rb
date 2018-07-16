281ff6bbbfebecc3f2b4b07bd854b1e71d820be3
Azwilson <azya.w99@gmail.com>
2018-07-16 13:54:00 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,10 @@
-# river.rb
+class River
+ 
+  def initialize (name, length, countries, discharge)
+  @name = name
+  @length = length
+  @countries = countries
+  @discharge = discharge
+  end
+  
+  def flood
